3d6378e18d1925d08b4748a532fc3e3fe2896bd3
Mohit Vaswani <mohitvaswani010@gmail.com>
2024-01-09 23:13:59 +0530

    tweet card added

--- app/[name]/page.tsx
+++ app/[name]/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import TwitterCard from '@/components/TwitterCard'
 
 const name = ({ params }: { params: { name: string } }) => {
     return (
-        <main className='p-5 flex justify-center items-center'>
+        <main className='p-5 flex flex-col justify-center items-center'>
             <h2> <span className='font-semibold'>{params.name}'s</span> Place</h2>
+            <TwitterCard/>
         </main>
     )
 }
---
+++ components/TwitterCard.tsx
@@ -0,0 +1,48 @@
+'use client'
+import { useState, FormEvent } from 'react';
+import { Tweet } from 'react-tweet';
+
+interface TwitterCardProps {}
+
+const TwitterCard: React.FC<TwitterCardProps> = () => {
+    const [tweetList, setTweetList] = useState<string[]>([]);
+    const [tweetLinkInput, setTweetLinkInput] = useState('');
+
+    const extractTweetId = (tweetLink: string): string | null => {
+        const tweetIdRegex = /\/status\/(\d+)/;
+        const match = tweetLink.match(tweetIdRegex);
+        return match ? match[1] : null;
+    };
+
+    const handleCreateTweet = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const tweetId = extractTweetId(tweetLinkInput);
+
+        if (tweetId) {
+            setTweetList((prevList) => [...prevList, tweetId]);
+            setTweetLinkInput(''); // Clear the input field
+        }
+    };
+
+    return (
+        <div className='w-10/12 flex flex-col'>
+            <form className='flex flex-col gap-1 w-5/12' onSubmit={handleCreateTweet}>
+                <input
+                    type="text"
+                    placeholder='Your Place'
+                    className='border-2 rounded-md p-2 bg-transparent outline-none'
+                    value={tweetLinkInput}
+                    onChange={(e) => setTweetLinkInput(e.target.value)}
+                />
+                <button type="submit" className='text-white p-2 px-4 rounded-md bg-black'>
+                    Create
+                </button>
+            </form>
+            {tweetList.map((tweetId) => (
+                <Tweet key={tweetId} id={tweetId} />
+            ))}
+        </div>
+    );
+};
+
+export default TwitterCard;
