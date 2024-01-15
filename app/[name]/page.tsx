371b74d796376d79ef277d5b1af1810dc6509fb1
Mohit Vaswani <mohitvaswani010@gmail.com>
2024-01-15 22:41:18 +0530

    some unuseful things removed

--- app/[name]/page.tsx
+++ app/[name]/page.tsx
@@ -1,15 +1,10 @@
 import React from 'react'
-import TwitterCard from '@/components/TwitterCard'
-import YoutubeCard from '@/components/YoutubeCard'
 
 const name = ({ params }: { params: { name: string } }) => {
     return (
         <main className='p-5 flex flex-col justify-center items-center'>
             <h2> <span className='font-semibold'>{params.name}'s</span> Place</h2>
-            <div className='flex gap-8 items-center w-full mt-5'>
-            <TwitterCard/>
-            <YoutubeCard/>
-            </div>
+            
         </main>
     )
 }
